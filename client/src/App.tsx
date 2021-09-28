26175ae6590b27c8002e725f3e3c02d9142007d6
PeacefulOtter <zankro67@gmail.com>
2021-09-28 09:43:56 +0200

    Finally added client

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Map from "./Components/Map"
+import "./App.css";
+
+interface RoadModel {
+  x : number,
+  y : number
+}
+
+function App() {
+  const [data, setData] = React.useState<RoadModel>({x: 0, y: 0});
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data as RoadModel);
+      })
+  }, []);
+
+  return (
+    <div className="App">
+      <Map position={data}/>
+      <p>{!data ? "Loading..." : data.x}</p>
+      <p>{!data ? "Loading..." : data.y}</p>
+    </div>
+  );
+}
+
+export default App;
