0d0cdf7dd1376e4e29abafce43c279518eb7a3e0
orhungorkem <gorkem.orhun98@gmail.com>
2021-10-05 21:17:45 +0200

    metadata listed and measurement types are passed to the component

--- client/src/Components/Rides/MetaDataTab.tsx
+++ client/src/Components/Rides/MetaDataTab.tsx
@@ -1,21 +1,24 @@
 import { FC, useState } from "react";
 
-import { Ride } from '../../assets/models'
+import { MeasurementsModel, Ride } from '../../assets/models'
 import MetaData from "./MetaData";
 import '../../css/ridecard.css'
 
 type Props = {
     ride: Ride,
+    measurementTypes: MeasurementsModel
     //index: number,
     //onClick: () => void
 };
 
-const MetaDataTab: FC<Props> = ( { ride } ) => {  //metadata component is put, then measurement types should be listed in a checkbox
+const MetaDataTab: FC<Props> = ( { ride, measurementTypes } ) => {  //metadata component is put, then measurement types should be listed in a checkbox
     // FIXME: replace ride with rideMeta as props
     return (
         <div className="ride-metadata-container" >
             <MetaData data = {ride.meta} key={`ride${ride}`}></MetaData>
+            <div>{measurementTypes}</div>
         </div>
+        
     
   )
 }
