f06d5374b40de7cb796129444a9d57fbb2bcc747
PeacefulOtter <zankro67@gmail.com>
2022-05-18 11:41:30 +0200

    popup when data.length = 0 is a toast

--- client/src/Components/Rides/Rides.tsx
+++ client/src/Components/Rides/Rides.tsx
@@ -28,41 +28,41 @@ const Rides: FC = () => {
         <ZoomProvider>
         <GraphProvider>
             <div className="map-container">
-            <MapWrapper>
-                { _metas.map( (meta: RideMeta, i: number) =>
-                    _measurements.map( (meas: RideMeasurement, j: number) =>
-                        <RidePath 
-                            key={`RidePath-${i}-${j}`}
-                            meas={meas}
-                            meta={meta} 
+                <MapWrapper>
+                    { _metas.map( (meta: RideMeta, i: number) =>
+                        _measurements.map( (meas: RideMeasurement, j: number) =>
+                            <RidePath 
+                                key={`RidePath-${i}-${j}`}
+                                meas={meas}
+                                meta={meta} 
+                            />
+                        )
+                    ) }
+                </MapWrapper>
+                {
+                    _measurements.map( (meas: RideMeasurement, i: number) => 
+                        meas.hasValue && <Graph 
+                            key={"graph-"+i}
+                            labelX="time (?)" 
+                            labelY={meas.name}
+                            plots={ 
+                                paths.hasOwnProperty(meas.name) 
+                                    ? Object.entries(paths[meas.name]).map( ([taskId, dp], i: number) => {
+                                        const { path, bounds } = dp;
+
+                                        const data: GraphData = path.map( (p: PointData, i:number) => 
+                                            [p.metadata.timestamp, p.value || 0, i]
+                                        )
+                                        console.log(bounds, data);
+                                        
+                                        return { data, bounds, label: 'r-' + taskId, i }
+                                    } ) 
+                                    : undefined 
+                            }
                         />
                     )
-                ) }
-            </MapWrapper>
-            {
-                _measurements.map( (meas: RideMeasurement, i: number) => 
-                    meas.hasValue && <Graph 
-                        key={"graph-"+i}
-                        labelX="time (?)" 
-                        labelY={meas.name}
-                        plots={ 
-                            paths.hasOwnProperty(meas.name) 
-                                ? Object.entries(paths[meas.name]).map( ([taskId, dp], i: number) => {
-                                    const { path, bounds } = dp;
-
-                                    const data: GraphData = path.map( (p: PointData, i:number) => 
-                                        [p.metadata.timestamp, p.value || 0, i]
-                                    )
-                                    console.log(bounds, data);
-                                    
-                                    return { data, bounds, label: 'r-' + taskId, i }
-                                } ) 
-                                : undefined 
-                        }
-                    />
-                )
-            }
-        </div>
+                }
+            </div>
         </GraphProvider>
         </ZoomProvider>
   )
