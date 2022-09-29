b3851b86b1726afd4037e301f7ffb97738c70e96
Laura <laura97sh@gmail.com>
2022-09-29 21:13:32 +0200

    Added support for Firebase integration (requires developers to update their environment variables)

---
+++ server-nest/src/firebase.ts
@@ -0,0 +1,13 @@
+﻿import * as firebase from "firebase/app";
+import 'firebase/firestore';
+
+import { FIREBASE_CONFIG } from './database';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+
+const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
+
+export const firestore = getFirestore(firebaseApp);
+
+export const auth = getAuth(firebaseApp);
