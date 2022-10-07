ee91b7b9c6d3f3855ee0b58592fe6ae938731b5b
Jonas M Peltonen <s204442@student.dtu.dk>
2022-10-07 12:55:27 +0200

    Connection service now connected to new connection component that shows the knex connection status. Self-updating every 10 seconds + 30 second timeout wait for knex. (1/2)

---
+++ client/src/components/Connection/Connection.tsx
@@ -0,0 +1,35 @@
+import { FC, useEffect, useState } from "react";
+
+import { getConnection } from "../../queries/connection";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import PendingIcon from '@mui/icons-material/Pending';
+import { green, yellow, red } from '@mui/material/colors'
+
+const Connection: FC = () => {
+
+    const [ connection, setConnection ] = useState<any>(null)
+
+    useEffect(() => {
+        const id = setInterval(() => getConnection(setConnection), 10000);
+
+        return () => {
+            clearInterval(id);
+        };
+    }, []);
+
+    if (connection === true) return (
+        <CheckCircleOutlineIcon sx={{ m: 1, color: green[500] }}>
+        </CheckCircleOutlineIcon>
+    )
+    else if (connection === false) return (
+        <ErrorOutlineIcon sx={{ m: 1, color: red[500] }}>
+        </ErrorOutlineIcon>
+    )
+    else return (
+        <PendingIcon sx={{ m: 1, color: yellow[500]}}>
+        </PendingIcon>
+    )
+}
+
+export default Connection;
