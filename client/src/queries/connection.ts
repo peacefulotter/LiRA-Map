a209f8f9f9e79192674a9a69fa240246712ff9a8
Zarkino <fhv2325@gmail.com>
2022-10-25 11:08:08 +0200

    Change connection type from any to boolean or null and change return type of getConnection

--- client/src/queries/connection.ts
+++ client/src/queries/connection.ts
@@ -1,6 +1,6 @@
 import { get } from "./fetch";
 
-export const getConnection = ( callback: React.Dispatch<React.SetStateAction<boolean>> ) => {
+export const getConnection = ( callback: React.Dispatch<React.SetStateAction<boolean | null>> ) => {
     get('/connection', (connection: boolean) => {
         console.log("Connection:\n");
         console.log(connection);
