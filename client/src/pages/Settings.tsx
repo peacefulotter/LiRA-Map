cd2cda22d56325cf2c90b0368b11e75b9137f518
Zarkino <fhv2325@gmail.com>
2022-10-28 23:42:07 +0200

    Fixed the theme switcher Changed onChangedMode to onToggleMode. Added section for deleting your account. Cleaned up imports.

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+// MUI
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
@@ -10,11 +10,12 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+// Components
 import {MaterialUISwitch} from '../components/MaterialUISwitch';
 import {useSelector} from "react-redux";
 import {RootState} from "../store";
 import useSettings from "../hooks/useSettings";
-import SettingsMode from '../components/settings/drawer/SettingMode'
+import {useState, SyntheticEvent} from "react";
 
 const languages = [
     { label: 'Danish', id: 1 },
@@ -23,81 +24,82 @@ const languages = [
 
 const Settings = () => {
     // Theme Switch
-    const { themeMode, onChangeMode } = useSettings();
+    const { themeMode, onToggleMode } = useSettings();
 
     const isDark = themeMode === 'dark';
 
     // Tab Context
-    const [value, setValue] = React.useState('1');
+    const [value, setValue] = useState<string>("1");
 
-    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-        setValue(newValue);
-    };
-
-    // User information
+    // User Information
     const { userData, userCredentials } = useSelector((state: RootState) => state.access)
 
     return (
         <Box sx={{ width: '100%', margin: '5px', typography: 'body1' }}>
             <TabContext value={value}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabList onChange={handleChange}>
-                        <Tab label="Profile" value="1" />
-                        <Tab label="Account" value="2" />
-                        <Tab label="Personalization" value="3" />
+                    <TabList onChange={(event: SyntheticEvent, value: string) => setValue(value)}>
+                        <Tab value="1" label="Profile" />
+                        <Tab value="2" label="Account" />
+                        <Tab value="3" label="Personalization" />
                     </TabList>
                 </Box>
                 <TabPanel value="1">
                     <Stack spacing={2} alignItems="start">
                         <Typography variant="h5">General Info</Typography>
 
-                        <TextField id="outlined-basic" label="First Name" defaultValue={userData?.firstName} variant="outlined" />
+                        <Typography variant="overline">First Name</Typography>
+                        <TextField id="outlined-basic" placeholder="First Name" defaultValue={userData?.firstName} variant="outlined" />
 
-                        <Typography variant="overline" display="block">Last Name</Typography>
-                        <TextField id="outlined-basic" label="" defaultValue={userData?.lastName} variant="outlined" />
+                        <Typography variant="overline">Last Name</Typography>
+                        <TextField id="outlined-basic" placeholder="Last Name" defaultValue={userData?.lastName} variant="outlined" />
 
-                        <Typography variant="overline" display="block">Email</Typography>
-                        <TextField id="outlined-basic" label="" defaultValue={userCredentials?.user?.email} variant="outlined" />
+                        <Typography variant="overline">Email</Typography>
+                        <TextField id="outlined-basic" placeholder="Email" defaultValue={userCredentials?.user?.email} variant="outlined" />
 
                         <Button variant="contained">Save Information</Button>
                     </Stack>
                 </TabPanel>
                 <TabPanel value="2">
                     <Stack spacing={2} alignItems="start">
-                        <Typography variant="h5">Change Password</Typography>
+                        <Typography variant="h5">Password</Typography>
 
-                        <Typography variant="overline" display="block">Old Password</Typography>
-                        <TextField id="outlined-basic" label="" variant="outlined" />
+                        <Typography variant="overline">Old Password</Typography>
+                        <TextField id="outlined-basic" placeholder="Old Password" variant="outlined" />
 
-                        <Typography variant="overline" display="block">New Password</Typography>
-                        <TextField id="outlined-basic" label="" variant="outlined" />
+                        <Typography variant="overline">New Password</Typography>
+                        <TextField id="outlined-basic" placeholder="New Password" variant="outlined" />
 
-                        <Typography variant="overline" display="block">Confirm Password</Typography>
-                        <TextField id="outlined-basic" label="" variant="outlined" />
+                        <Typography variant="overline">Confirm Password</Typography>
+                        <TextField id="outlined-basic" placeholder="Confirm Password" variant="outlined" />
 
                         <Button variant="contained">Confirm</Button>
+
+                        <Typography variant="h5">Delete Account</Typography>
+                        <Typography>Would you like to delete your account?</Typography>
+                        <Typography>Deleting your account will remove all the content associated with it.</Typography>
+                        <Button variant="contained" color="error">I want to delete my account</Button>
                     </Stack>
                 </TabPanel>
                 <TabPanel value="3">
                     <Stack spacing={2} alignItems="start">
                         <Typography variant="h5">Preferences</Typography>
 
-                        <Typography variant="overline" display="block">Language selector</Typography>
+                        <Typography variant="overline">Language</Typography>
                         <Autocomplete
                             disablePortal
                             id="combo-box-lang"
                             options={languages}
                             sx={{ width: 300 }}
-                            renderInput={(params) => <TextField {...params} label="" />}
+                            renderInput={(params) => <TextField {...params} />}
                         />
-                        <Typography variant="overline" display="block">Theme switch</Typography>
+                        <Typography variant="overline">Theme</Typography>
                         <FormGroup>
                             <FormControlLabel
-                                control={<MaterialUISwitch sx={{ m: 1 }} checked={isDark} onChange={() => onChangeMode}/>}
-                                label={`${isDark ? 'Dark mode' : 'Light mode'}`}
+                                control={<MaterialUISwitch sx={{ m: 1 }} checked={isDark} onChange={onToggleMode}/>}
+                                label={isDark ? 'Dark mode' : 'Light mode'}
                             />
                         </FormGroup>
-                        <SettingsMode></SettingsMode>
                     </Stack>
                 </TabPanel>
             </TabContext>
