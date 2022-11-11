9549dc7091e4e4ee6d51a8b0b8f926f7c1dccff9
Emil-krarup <emil.krarup@live.dk>
2022-11-11 09:59:27 +0100

    including left side mu

--- client/src/Components/RoadMeasurements/FrictionCoefficient.tsx
+++ client/src/Components/RoadMeasurements/FrictionCoefficient.tsx
@@ -13,6 +13,8 @@ const [measurement, setMeasurement] = useState<ActiveMeasProperties>({});
 const RPM_Front_Right = "obd.rpm_fr";
 const RPM_Rear_Right = "obd.rpm_rr";
 const Car_Velocity = "obd.spd_veh";
+const RPM_Front_Left = "obd.rpm_fl";
+const RPM_Rear_Left = "obd.rpm_rl";
 
 const beta_front_right = 3 ;
 const beta_zero = 1;
@@ -31,14 +33,22 @@ export const mu_function = () => {
   const rpm_fr = chooseMeasurement(RPM_Front_Right) 
   const rpm_rr = chooseMeasurement(RPM_Rear_Right)
   const spd_veh = chooseMeasurement(Car_Velocity)
+  const rpm_fl = chooseMeasurement(RPM_Front_Left) 
+  const rpm_rl = chooseMeasurement(RPM_Rear_Left)
   
-  const mu_value = (Math.log((beta_front_right * wheel_radius)/(wheel_radius * rpm_fr - spd_veh)) + 1)
-  return addMeasurement(mu_value);
+  const mu_right_value = (Math.log((beta_front_right * wheel_radius)/(wheel_radius * rpm_fr - spd_veh)) + 1)
+  const mu_left_value = (Math.log((beta_front_right * wheel_radius)/(wheel_radius * rpm_fl - spd_veh)) + 1)
+  const mu_value = (mu_left_value+mu_right_value)/2
+  return writefile(mu_value);
   }
   )
  
   return ( 
     
   )
+}
+
+function writefile(mu_value: number): void | (() => void | { [UNDEFINED_VOID_ONLY]: never; }) {
+  throw new Error('Function not implemented.');
 }
 
