04c74eaf1a9ce1ac0a89539d4cd7e1933eaa38dd
Emil-krarup <emil.krarup@live.dk>
2022-11-18 14:05:30 +0100

    Updated mu_calculations
    It now has the proper math formula for calculation mu, also refactored names to be the proper term for variables

--- server-nest/src/mu/mu.service.ts
+++ server-nest/src/mu/mu.service.ts
@@ -21,15 +21,15 @@ export class MuService implements OnApplicationBootstrap {
         .select('*')
         .from({ public: 'Measurements' })
         .where({ FK_Trip: ride.TripId })
-        .whereIn('T', ['obd.rpm_fr', 'obd.spd'])
+        .whereIn('T', ['obd.rpm_fr', 'obd.rpm_rr'])
         .whereNot({ lat: null, lon: null })
         .orderBy('TS_or_Distance');
-      const rpms = data.filter((d) => d.T === 'obd.rpm_fr');
-      const spds = data.filter((d) => d.T === 'obd.spd');
+      const rpms_front = data.filter((d) => d.T === 'obd.rpm_fr');
+      const rpms_rear = data.filter((d) => d.T === 'obd.rpm_rr');
       const points: {
         MeasurementId: string;
         TS_or_Distance: Date;
-        T: 'mu';
+        T: 'mu_right';
         lat: number;
         lon: number;
         message: string;
@@ -40,57 +40,67 @@ export class MuService implements OnApplicationBootstrap {
         Updated_Date: any;
         Description: number;
       }[] = [];
-      for (let i = 0; i < Math.min(rpms.length / 5, spds.length); i++) {
-        const Speed = parseFloat(spds[i].Description);
-        const RPM = parseFloat(rpms[i * 5].Description);
-        const mu =
+      for (
+        let i = 0;
+        i < Math.min(rpms_front.length / 5, rpms_rear.length / 5);
+        i++
+      ) {
+        const RPM_rear = parseFloat(rpms_rear[i * 5].Description);
+        const RPM_front = parseFloat(rpms_front[i * 5].Description);
+        const mu_right =
           Math.log(
             (beta_front_right * wheel_radius) /
-              (wheel_radius * RPM - Speed / 3.6) +
+              (wheel_radius * (RPM_front - RPM_rear)) +
               1,
           ) ^
           (1 / beta__zero);
         const MeasurementId = randomUUID();
-        const spdMessage = JSON.parse(spds[i].message);
+        const spdMessage = JSON.parse(rpms_rear[i].message);
         const message = JSON.stringify({
           id: spdMessage.id,
           start_time_utc: spdMessage.start_time_utc,
           end_time_utc: spdMessage.end_time_utc,
           '@vid': spdMessage['@vid'],
           '@uid': MeasurementId,
-          '@ts': spds[i].TS_or_Distance,
-          '@t': 'mu',
-          'mu.value': mu,
+          '@ts': rpms_rear[i].TS_or_Distance,
+          '@t': 'mu_right',
+          'mu.value': mu_right,
           '@rec': spdMessage['@rec'],
         });
 
         //Logger.debug('\n Message', message);
-        if (isNaN(mu) || mu === Infinity || RPM === 0 || Speed === 0) continue;
+        if (
+          isNaN(mu_right) ||
+          mu_right === Infinity ||
+          RPM_front === 0 ||
+          RPM_rear === 0
+        )
+          continue;
 
         points.push({
           MeasurementId: MeasurementId,
-          TS_or_Distance: spds[i].TS_or_Distance,
-          T: 'mu',
-          lat: spds[i].lat,
-          lon: spds[i].lon,
+          TS_or_Distance: rpms_rear[i].TS_or_Distance,
+          T: 'mu_right',
+          lat: rpms_rear[i].lat,
+          lon: rpms_rear[i].lon,
           message: message,
           isComputed: true,
           FK_Trip: ride.TripId,
           FK_MeasurementType: '59644af0-34da-4f03-b08c-456d38b0c56e',
-          Created_Date: spds[i].Created_Date,
-          Updated_Date: spds[i].Updated_Date,
-          Description: mu,
+          Created_Date: rpms_rear[i].Created_Date,
+          Updated_Date: rpms_rear[i].Updated_Date,
+          Description: mu_right,
         });
         //Logger.debug('\n Updated Date', spds[i].Updated_Date);
         //Logger.debug('FK_Trip', ride.TripId);
-        if (mu < 0)
+        if (mu_right < 0)
           Logger.debug(
             'Speed \t' +
-              spds[i].Description +
+              rpms_rear[i * 5].Description * wheel_radius +
               '\n RPM \t' +
-              rpms[i * 5].Description +
+              rpms_front[i * 5].Description +
               '\n Mu \t' +
-              mu +
+              mu_right +
               '\n',
           );
       }
